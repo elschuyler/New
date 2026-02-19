6a64c590a36b788ad3be2fa49027089ddc928d06
elschuyler <elvilewis40@gmail.com>
2026-02-19 14:25:59 +0530

    Update PluginsActivity.kt

--- VibeForgeLauncher(w)/app/src/main/java/com/vibeforge/launcher/ui/PluginsActivity.kt
+++ VibeForgeLauncher(w)/app/src/main/java/com/vibeforge/launcher/ui/PluginsActivity.kt
@@ -3,35 +3,23 @@ package com.vibeforge.launcher.ui
 import android.app.ActivityManager
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.vibeforge.launcher.IVibeforgePlugin
 import com.vibeforge.launcher.R
-import com.vibeforge.launcher.core.PluginManager
 
 class PluginsActivity : AppCompatActivity() {
 
-    private lateinit var pluginManager: PluginManager
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plugins)
 
-        pluginManager = PluginManager(this)
-        pluginManager.discoverAndBindPlugins()
-
         recyclerView = findViewById(R.id.pluginsList)
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-        recyclerView.postDelayed({
-            recyclerView.adapter = PluginAdapter(pluginManager.getActivePlugins())
-        }, 1000)
+        recyclerView.adapter = EmptyPluginAdapter()
 
         val memText = findViewById<TextView>(R.id.systemMemoryText)
         val actManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
@@ -41,27 +29,15 @@ class PluginsActivity : AppCompatActivity() {
     }
 }
 
-class PluginViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val name: TextView = view.findViewById(R.id.pluginName)
-    val stats: TextView = view.findViewById(R.id.pluginStats)
-}
-
-class PluginAdapter(private val plugins: List<IVibeforgePlugin>) : RecyclerView.Adapter<PluginViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PluginViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_plugin, parent, false)
-        return PluginViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: PluginViewHolder, position: Int) {
-        val plugin = plugins[position]
-        try {
-            holder.name.text = plugin.pluginName
-            holder.stats.text = "Version: ${plugin.pluginVersion} | RAM: ${plugin.memoryUsage / 1024} KB"
-        } catch (e: Exception) {
-            holder.name.text = "Link Broken"
-        }
-    }
-
-    override fun getItemCount() = plugins.size
+class EmptyPluginAdapter : androidx.recyclerview.widget.RecyclerView.Adapter<androidx.recyclerview.widget.RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: android.view.ViewGroup, viewType: Int) =
+        object : androidx.recyclerview.widget.RecyclerView.ViewHolder(
+            android.widget.TextView(parent.context).apply {
+                text = "No plugins installed yet."
+                setPadding(32, 32, 32, 32)
+                setTextColor(android.graphics.Color.WHITE)
+            }
+        ) {}
+    override fun onBindViewHolder(holder: androidx.recyclerview.widget.RecyclerView.ViewHolder, position: Int) {}
+    override fun getItemCount() = 1
 }
