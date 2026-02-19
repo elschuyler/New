25da511463215df6544ee6fc2eb0f68e963a6c75
elschuyler <elvilewis40@gmail.com>
2026-02-19 15:53:26 +0530

    Update AppLockManager.kt

--- VibeForgeLauncher(w)/app/src/main/java/com/vibeforge/launcher/core/AppLockManager.kt
+++ VibeForgeLauncher(w)/app/src/main/java/com/vibeforge/launcher/core/AppLockManager.kt
@@ -1,27 +1,16 @@
 package com.vibeforge.launcher.core
 
 import android.content.Context
-import com.vibeforge.launcher.data.AppDatabase
-import com.vibeforge.launcher.data.LockedApp
-import androidx.room.Room
 
 class AppLockManager(context: Context) {
-    private val db = Room.databaseBuilder(
-        context.applicationContext,
-        AppDatabase::class.java, "vibeforge-db"
-    ).build()
+    private val lockedApps = mutableSetOf<String>()
 
-    private val dao = db.appLockDao()
-
-    suspend fun isLocked(packageName: String): Boolean {
-        return dao.isAppLocked(packageName)
+    fun isLocked(packageName: String): Boolean {
+        return lockedApps.contains(packageName)
     }
 
-    suspend fun setLocked(packageName: String, locked: Boolean) {
-        if (locked) {
-            dao.lockApp(LockedApp(packageName))
-        } else {
-            dao.unlockApp(LockedApp(packageName))
-        }
+    fun setLocked(packageName: String, locked: Boolean) {
+        if (locked) lockedApps.add(packageName)
+        else lockedApps.remove(packageName)
     }
 }
